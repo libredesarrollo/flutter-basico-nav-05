41430add3628228412a5d70f3a995396cd7b8458
Andres <andres29111990@gmail.com>
2021-01-07 10:43:19 -0400

    Proyecto navegación y rutas básicas

---
+++ lib/pages/one.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:nav_05/pages/two.dart';
+import 'package:nav_05/widgets/botones_nav.dart';
+
+class One extends StatelessWidget {
+  static const String ROUTE = '/one';
+
+  @override
+  Widget build(BuildContext context) {
+    String text = ModalRoute.of(context).settings.arguments;
+
+    if(text == null){
+      text = "Default Uno";
+    }
+
+    return BotonesNav(
+      text: text,
+    );
+  }
+}
